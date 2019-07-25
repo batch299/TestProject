c88082db7ce9dc285564679ab4046adf944ebd2a
batch299 <krishnamohanvenkat9@gmail.com>
2019-07-25 19:39:13 +0530

    sample prog

---
+++ TestProject/src/Sample.java
@@ -0,0 +1,19 @@
+
+public class Sample 
+{
+
+	int a,b;
+	public void add()
+	{
+		System.out.println(a+b+c);
+	}
+	public static void main(String[] args) 
+	{
+		Sample s=new Sample();
+		s.a=5;
+		s.b=20;
+		s.add();
+
+	}
+
+}
